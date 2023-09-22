7103563717a2a6f0e811932e7029f0696be98181
corner <corner3499@gmail.com>
2023-09-22 18:18:39 +0900

    config fix

--- next.config.js
+++ next.config.js
@@ -3,14 +3,6 @@ const nextConfig = {
     images: {
         domains: ['localhost', 'unsplash.com', '210.90.202.90']
     },
-    async rewrites() {
-        return [
-            {
-                source: '/api/:path*',
-                destination: 'http://15.164.190.1:8080/:path*'
-            }
-        ];
-    }
 };
 
 module.exports = nextConfig;
