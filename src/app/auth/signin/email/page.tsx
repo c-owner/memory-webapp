d2e0af50b48874fd51f50d59c19f9713e4d1f2db
corner <corner3499@gmail.com>
2023-09-22 16:30:19 +0900

    redirection

--- src/app/auth/signin/email/page.tsx
+++ src/app/auth/signin/email/page.tsx
@@ -1,6 +1,15 @@
 import EmailLogin from '@/components/auth/EmailLogin';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/pages/api/auth/[...nextauth]';
+import { redirect } from 'next/navigation';
 
-export default function EmailPage() {
+export default async function EmailPage() {
+    console.log('??????');
+    const session = await getServerSession(authOptions);
+    if (session) {
+        redirect('/');
+    }
+
     return (
         <section className="w-full py-24 grid place-items-center h-full">
             <EmailLogin />
