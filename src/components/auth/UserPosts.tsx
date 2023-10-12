7c0a1ed779bc224e13e5e64800f1328c9eb84bf2
corner <corner3499@gmail.com>
2023-10-12 21:25:55 +0900

    sidebar 제거

--- src/components/auth/UserPosts.tsx
+++ src/components/auth/UserPosts.tsx
@@ -6,7 +6,6 @@ import { useState } from 'react';
 import BookmarkIcon from '@/components/ui/icon/BookmarkIcon';
 import PostIcon from '@/components/ui/icon/PostIcon';
 import PostList from '@/components/list/PostList';
-import SideBar from '@/components/SideBar';
 
 type Props = {
     user: ProfileUser;
@@ -22,7 +21,7 @@ export default function UserPosts({ user, mySession }: Props) {
     // TODO :: 나의 멤버아이디와 유저아이디가 다르면 나의 게시물이 아님
     return (
         <section>
-            <ul className="flex justify-center uppercase">
+            <ul className="w-full flex justify-center uppercase">
                 {user.id !== mySession.id ? (
                     <li className="mx-12 p-4 cursor-pointer border-black dark:border-white font-bold border-t">
                         <button className="scale-150 md:scale-100">{tabs[0].icon}</button>
@@ -54,7 +53,6 @@ export default function UserPosts({ user, mySession }: Props) {
                     <div className="w-full basis-3/4 min-w-0">
                         <PostList user={user} />
                     </div>
-                    <div className="basis-1/4 ml-8">{user && <SideBar user={user} />}</div>
                 </section>
             </CacheKeysContext.Provider>
         </section>
